c3daed7ccfca3e317f3570b143d4254dd9e8a82f
pitao <pitaohc@qq.com>
2023-11-30 20:36:54 +0800

    12.2.读取文件

--- Chapter12/minigrep/src/main.rs
+++ Chapter12/minigrep/src/main.rs
@@ -1,4 +1,5 @@
-use std::env; // 引入标准库中的env模块
+use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // 获取命令行参数
@@ -10,10 +11,19 @@ fn main() {
     args 无法处理非法的Unicode字符
     如果需要处理非法的Unicode字符，可以使用std::env::args_os // OsString
     */
-    // println!("{:?}", args); // 打印命令行参数
+    println!("{:?}", args); // 打印命令行参数
 
     let query = &args[1];
     let filename = &args[2];
     println!("Searching for \"{}\"", query);
     println!("In file \"{}\"", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file\n");
+    /*
+    read_to_string()返回一个Result<String, io::Error>类型
+    如果Result是Err，expect()会导致程序崩溃，并显示expect()的参数
+    如果Result是Ok，expect()会获取Ok中的值并返回
+    */
+    println!("With text:\n{}", contents);
 }
