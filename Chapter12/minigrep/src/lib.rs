39247fb65632631566535836da43a2a8c466dade
pitao <pitaohc@qq.com>
2023-12-04 23:32:56 +0800

    12.6.测试驱动开发，编写test函数，编写search函数

--- Chapter12/minigrep/src/lib.rs
+++ Chapter12/minigrep/src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     /*
     Box<dyn Error>是一个trait对象，它可以表示任何实现Error trait的类型
@@ -8,7 +9,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     /*
     ?运算符会将Result中的值取出并返回，如果Result是Err，?运算符会提前返回整个函数
     */
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
 
@@ -34,4 +37,34 @@ impl Config {
 pub struct Config {
     pub query: String,
     pub filename: String,
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = vec![];
+    for line in contents.lines(){
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        /*
+        先编写测试，测试会失败。
+        再修改函数功能，让函数符合测试，类似算法题的思路。
+        */
+        assert_eq!(vec!["safe, fast, productive."],
+                   search(query, contents));
+    }
 }
