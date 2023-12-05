f930d7d8d8b8e017b435e9224e66d4e08ac60a8e
pitao <pitaohc@qq.com>
2023-12-05 19:30:20 +0800

    12.7.环境变量，配置选项，大小写敏感, 删除前几节的注释

--- Chapter12/minigrep/src/lib.rs
+++ Chapter12/minigrep/src/lib.rs
@@ -1,25 +1,21 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    /*
-    Box<dyn Error>是一个trait对象，它可以表示任何实现Error trait的类型
-    */
     let contents = fs::read_to_string(config.filename)?;
-    /*
-    ?运算符会将Result中的值取出并返回，如果Result是Err，?运算符会提前返回整个函数
-    */
-    for line in search(&config.query, &contents) {
+    let result = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in result {
         println!("{}", line);
     }
     Ok(())
 }
 
 impl Config {
-    /*
-    使用Result来处理错误
-    &‘static str是字符串字面值的类型，它的生命周期是静态的
-    */
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
@@ -27,22 +23,66 @@ impl Config {
         let query = &args[1];
         let filename = &args[2];
         /*
-        不能夺取query和filename的所有权，因为parse_config()需要返回Config实例
-        to_string()会从&str创建一个String
+        env::var 返回一个Result, 如果环境变量存在，返回Ok，否则返回Err
+        is err 检查Result是否是Err, 如果是Err返回true，否则返回false
+        CASE_INSENSITIVE 不区分大小写的环境变量
         */
-        Ok(Config { query: query.to_string(), filename: filename.to_string() })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config { query: query.to_string(), filename: filename.to_string(), case_sensitive })
     }
 }
 
+
+/// Config结构体
+///
+/// query: String, 查询字符串
+/// filename: String, 文件名
+/// case_sensitive: bool, 是否区分大小写
+///
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
+/// 大小写敏感的搜索字符串函数
+///
+/// # Arguments
+///
+/// * `query`: 查询字符串
+/// * `contents`: 文件内容
+///
+/// returns: Vec<&str, Global> 包含查询字符串的行
+///
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     let mut result: Vec<&str> = vec![];
-    for line in contents.lines(){
-        if line.contains(query){
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+/// 大小写不敏感的搜索字符串函数
+///
+/// # Arguments
+///
+/// * `query`: 查询字符串
+/// * `contents`: 文件内容
+///
+/// returns: Vec<&str, Global> 包含查询字符串的行
+///
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    /*
+    将query转换为小写,新query拥有所有权
+    不能在此调用as_str, 因为to_lowercase()返回的String会在此行调用结束后被删除
+    */
+    let query = query.to_lowercase();
+
+    let mut result: Vec<&str> = vec![];
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             result.push(line);
         }
     }
@@ -54,17 +94,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
-        /*
-        先编写测试，测试会失败。
-        再修改函数功能，让函数符合测试，类似算法题的思路。
-        */
+Pick three.
+Duct";
+
         assert_eq!(vec!["safe, fast, productive."],
                    search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct";
+
+        assert_eq!(vec!["safe, fast, productive.", "Duct"],
+                   search_case_insensitive(query, contents));
     }
 }
--- Chapter12/minigrep/src/main.rs
+++ Chapter12/minigrep/src/main.rs
@@ -1,50 +1,15 @@
 use std::{env, process};
 use minigrep::Config;
-/*
-二进制程序关注点分离的指导性原则
-将程序拆分为main.rs和lib.rs并将程序的逻辑放入lib.rs中
-当命令行解析逻辑较少时，可以将解析逻辑放入main.rs中
-当命令行解析逻辑较多时，需要将解析逻辑放入lib.rs中
-*/
-/*
-经过上述拆分，留在main.rs中的代码有：
-- 使用参数值调用命令行解析逻辑
-- 调用lib.rs中的run函数
-- 处理run函数可能出现的错误
-*/
-/*
-测试驱动开发
-- 编写一个会失败的测试，运行该测试，确保它按照预期的原因失败
-- 编写或修改刚好足够的代码，让新测试通过
-- 重构刚刚的添加或修改的代码，确保测试始终通过
-- 重复上述步骤
-*/
-fn main() {
-    let args: Vec<String> = env::args().collect(); // 获取命令行参数
-    /*
-    args()返回一个迭代器
-    collect()将迭代器转换为一个集合
-    */
-    /*
-    args 无法处理非法的Unicode字符
-    如果需要处理非法的Unicode字符，可以使用std::env::args_os // OsString
-    */
 
-    /*
-    unwrap_or_else()是Result类型的一个方法
-    如果Result是Ok，unwrap_or_else()会获取Ok中的值并返回
-    如果Result是Err，unwrap_or_else()会调用闭包并将Err中的值作为参数传递给闭包
-    */
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1); // 退出程序并返回状态码1
     });
 
     if let Err(e) = minigrep::run(config) {
-        /*
-        if let 功能类似于match，但是只匹配一个模式
-        此处这么使用因为run如果成功返回的是一个空元组，如果失败返回的是一个Box<dyn Error>
-        */
         println!("Application error: {}", e);
         process::exit(1);
     }
